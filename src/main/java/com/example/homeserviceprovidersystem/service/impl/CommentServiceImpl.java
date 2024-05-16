f3663d4f379e22a0102c1d0776b7139268196a0f
mehdi <mehdiarghan@gmail.com.git/>
2024-05-16 16:03:12 +0330

    added CommentServiceImpl

---
+++ src/main/java/com/example/homeserviceprovidersystem/service/impl/CommentServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.homeserviceprovidersystem.service.impl;
+
+import com.example.homeserviceprovidersystem.service.CommentsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentServiceImpl implements CommentsService {
+}
